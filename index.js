13a5ee40c777f92a764d8b47e8d18382caaa6723
Sangvinijus A. Rinkevičius <116754029+Sangvinijus@users.noreply.github.com>
2022-12-18 22:23:35 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function newImg1(e){e.src="https://raw.githubusercontent.com/Sangvinijus/sangvinijus.github.io/main/.images/Menu%20Button%201.2.png"}
-function oldImg1(e){e.src="https://raw.githubusercontent.com/Sangvinijus/sangvinijus.github.io/main/.images/Menu%20Button%201.png"}
-
-function newImg2(e){e.src="https://raw.githubusercontent.com/Sangvinijus/sangvinijus.github.io/main/.images/Menu%20Button%202.2.png"}
-function oldImg2(e){e.src="https://raw.githubusercontent.com/Sangvinijus/sangvinijus.github.io/main/.images/Menu%20Button%202.png"}
-
-function newImg3(e){e.src="https://raw.githubusercontent.com/Sangvinijus/sangvinijus.github.io/main/.images/Menu%20Button%203.2.png"}
-function oldImg3(e){e.src="https://raw.githubusercontent.com/Sangvinijus/sangvinijus.github.io/main/.images/Menu%20Button%203.png"}
